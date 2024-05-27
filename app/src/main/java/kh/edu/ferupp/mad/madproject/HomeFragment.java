f1e1a1c3ead42be12d772476de712589de64e4bb
Chamroeunpiseth Thy <thy.chamroeunpiseth.2821@rupp.edu.kh>
2024-05-27 20:07:15 +0700

    Route menu to all fragment pages

--- app/src/main/java/kh/edu/ferupp/mad/madproject/HomeFragment.java
+++ app/src/main/java/kh/edu/ferupp/mad/madproject/HomeFragment.java
@@ -2,21 +2,34 @@ package kh.edu.ferupp.mad.madproject;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.appcompat.app.AppCompatActivity;
 
 import kh.edu.ferupp.mad.madproject.databinding.FragmentHomeBinding;
 
 
 public class HomeFragment extends Fragment {
 
+//    private FragmentHomeBinding binding;
+    @Nullable
     @Override
     public View onCreateView(@Nullable LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState) {
         FragmentHomeBinding binding = FragmentHomeBinding.inflate(inflater,container,false);
         return binding.getRoot();
+
     }
+//    @Override
+//    public void onDestroxyView() {
+//        super.onDestroyView();
+//        binding = null;
+//    }
+
+
+
 }
--- app/src/main/java/kh/edu/ferupp/mad/madproject/MainActivity.java
+++ app/src/main/java/kh/edu/ferupp/mad/madproject/MainActivity.java
@@ -8,42 +8,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import kh.edu.ferupp.mad.madproject.databinding.ActivityMainBinding;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    private static final int MENU_HOME_ID = R.id.menuHome;
+    private static final int MENU_MEETING_ID = R.id.menuMeeting;
+    private static final int MENU_ADD_ID = R.id.menuAdd;
+
+    private static final int MENU_CALENDAR_ID = R.id.menuCalender;
+    private static final int MENU_PROFILE_ID = R.id.menuProfile;
+
     @Override
-    protected void onCreate(@Nullable Bundle saveInstanceState) {
-        super.onCreate(saveInstanceState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         showFragment(new HomeFragment());
 
         binding.bottomNavigationView.setOnItemSelectedListener(menuItem -> {
-            if (menuItem.getItemId() == R.id.menuHome) {
+            int itemId = menuItem.getItemId(); // Get the item ID once to avoid multiple calls
+
+            // Use if-else instead of switch-case
+            if (itemId == MENU_HOME_ID) {
                 showFragment(new HomeFragment());
                 return true;
-            } else if (menuItem.getItemId() == R.id.menuMeeting) {
+            } else if (itemId == MENU_MEETING_ID) {
                 showFragment(new MeetingFragment());
                 return true;
-            } else if (menuItem.getItemId() == R.id.menuCalender) {
-                showFragment(new CalendarFragment());
+            } else if (itemId == MENU_ADD_ID) {
+                showFragment(new AddFragment());
                 return true;
-            } else if (menuItem.getItemId() == R.id.profile) {
-                showFragment(new HomeFragment());
+            } else if (itemId == MENU_CALENDAR_ID) {
+                showFragment(new ScheduleFragment());
                 return true;
+            } else if (itemId == MENU_PROFILE_ID) {
+                showFragment(new ProfileFragment());
+                return true;
+            } else {
+                return false;
             }
-            return true;
         });
     }
 
+
     private void showFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(binding.frameLayout.getId(),fragment);
-        fragmentTransaction.commit();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frameLayout, fragment)
+                .commit();
     }
 }
-
